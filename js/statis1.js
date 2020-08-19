89741b2da5ba8581ea9503757a28e47f6ae69b6c
yupzhou <yupzhou@foxmail.com>
2020-08-19 23:26:19 +0800

    200819 1.4a

---
+++ js/statis1.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?4b2c3f4306a49fe61230fbc06f558987";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
---
+++ js/statis3.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?9c87f75b4219517e7fa788e8c20b8351";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
